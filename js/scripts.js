ecac82abea0c221095f106b7550c60e33bc20ba6
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-01 16:49:40 -0700

    create directories
    co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+let sent = prompt("Write a single sentence."); {
+
+}
+let char1 = sent.charAt(0); {
+
+}
+let char2 = sent.charAt(sent.length-1); {
+
+}
+let char3 = char1 + char2; {
+
+}
+let char4 = char3.toUpperCase(); {
+  char4;
+}
